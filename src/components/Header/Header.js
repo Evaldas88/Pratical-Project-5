ea7bf7bc5ee95e4e5ace5a07c58fcb5ae45297c6
Evaldas88 <e.skackauskas@gmail.com>
2022-08-20 11:39:44 +0300

    Created NavLink, added boostrap styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,44 @@
+import { NavLink } from "react-router-dom";
 import React from 'react';
-import PropTypes from 'prop-types';
 import './Header.css';
 
-const Header = () => (
-  <div className="Header" data-testid="Header">
-    Header Component
-  </div>
-);
 
-Header.propTypes = {};
-
-Header.defaultProps = {};
+function Header() {
+  return (
+    <nav className="navbar justify-content-center navbar-expand-lg navbar-light bg-light ">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div
+        className="navbar-collapse collapse "
+        id="navbarNav"
+      >
+        <ul className="navbar-nav">
+        
+          <li className="nav-item">
+            <NavLink className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link" } 
+                to="/">List
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link" } 
+                to="/about">About
+            </NavLink>
+          </li>    
+        </ul>
+      </div>
+    </nav>
+  );
+}
 
 export default Header;
