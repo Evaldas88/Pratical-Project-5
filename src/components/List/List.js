ebb982324d2dccd5ab97e987740c89bc73ad65d7
Evaldas88 <e.skackauskas@gmail.com>
2022-08-22 14:42:14 +0300

    fixed useEffect, now list can load from local storage

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,48 +1,56 @@
 import React, { useState, useRef, useEffect } from "react";
 
 const List = () => {
+  
   const [newItem, setNewItem] = useState({});
   const [items, setItems] = useState([]);
   const itemInput = useRef(null);
 
+// Affter reaload page takes info from localStorage
+
   useEffect(() => {
-    const storageList = JSON.parse(localStorage.getItem("items"));
-    if (storageList) {
-      setItems(storageList);
-    }
+    const storageList  = JSON.parse(localStorage.getItem("items"));
+    if (storageList){
+    setItems(storageList);
+  }
   }, []);
+
+  // Adding imput value in localStorage
 
   useEffect(() => {
     localStorage.setItem("items", JSON.stringify(items));
   }, [items]);
 
+// Delete items value 
 
   const deleteItem = (id) => {
     const temItems = items.slice();
-    const index = temItems.findIndex(items => items.id === id);
+    const index = temItems.findIndex(idx => idx !== id);
     temItems.splice(index, 1)
 
     setItems(temItems);
 
   };
 
+
+
   const handleInput = (e) => {
-    e.preventDefault();
     setNewItem({ name: e.target.value });
   };
 
+//
+
   const handleClick = (e) => {
     e.preventDefault();
-
     itemInput.current.value = "";
-    setItems([...items, newItem]);
+    setItems([newItem, ...items]);
   };
 
   return (
     <div className="container">
       <div className="card w-25 text-bg-light">
         <div className="card-body">
-          <h1 className="card-title">Items</h1>
+          <h1 className="card-title">To do list</h1>
           <div className="row p-3">
             <input
               id="input"
@@ -57,10 +65,10 @@ const List = () => {
           </div>
           <ul className="list-group">
             {items.length > 0 ? (
-              items.map((items, id) => (
-                <li key={items.id} className="list-group-item">
+              items.map((items, idx) => (
+                <li key={idx} className="list-group-item">
                   {items.name}
-                  <button className="btn btn-warning float-end" onClick={() => { deleteItem(id); }}>
+                  <button className="btn btn-warning float-end" onClick={() => { deleteItem(idx); }}>
                     Delete
                   </button>
                 </li>
