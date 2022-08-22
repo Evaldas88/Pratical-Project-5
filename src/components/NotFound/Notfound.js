d876c71271c7775d89f98b476d4225f11585a2f7
Evaldas88 <e.skackauskas@gmail.com>
2022-08-22 19:00:01 +0300

    created NotFound

---
+++ src/components/NotFound/Notfound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MyImage from '../Images/1.png'
+
+const NotFound = () => (
+  <div className="text-center">
+    <img src={MyImage} alt='Error' className='error' />
+  </div>
+);
+
+
+export default NotFound;
