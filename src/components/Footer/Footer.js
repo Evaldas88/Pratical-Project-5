0f6134a34a786caadefa834ff6a0297102d276cb
Evaldas88 <e.skackauskas@gmail.com>
2022-08-24 17:36:37 +0300

    Added footer to bottom

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import './Footer.css';
 
 const Footer = () => (
   <div className="main-footer">
-      <div className='text-center p-1' >
-        &copy; {new Date().getFullYear()} Copyright:{' '}
 
-        <p>Evaldas Skačkauskas</p>
-      </div>
+      <footer className=" text-center ">
+        <div className="container p-1">
+          <section className="mb-1">
+            <a
+              className="btn  btn-secondary btn-floating m-1"
+              href="https://github.com/Evaldas88"
+              role="button"
+            >
+             <i className="bi bi-github"></i>
+            </a>
+          </section>
+          </div>
+      <div className='text-center p-1 ' >
+        &copy; {new Date().getFullYear()} Copyright: Evaldas88
+  </div>
+  </footer>
   </div>
 );
 
